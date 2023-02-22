493097df2474ea2df08698a9a5991b27847b2618
Mark Graham-Smith <markgraham-smith@Marks-MacBook-Pro-2.local>
2023-02-22 21:47:31 +0000

    Add Todo Controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index() {
+
+    }
+
+    public funciton upsert() {
+
+    }
+
+    public function destroy() {
+
+    }
+}
