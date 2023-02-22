ae5659d9009744afda16e36ba4d2b94a03720362
Mark Graham-Smith <markgraham-smith@Marks-MacBook-Pro-2.local>
2023-02-22 23:35:31 +0000

    List display style

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,17 @@
     <meta charset="UTF-8" />
     <title>Todo Application</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            safelist: [
+                'rounded-t',
+                'rounded-b',
+                'border-t-0'
+            ]
+        }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
--- resources/views/todo-list.blade.php
+++ resources/views/todo-list.blade.php
@@ -3,9 +3,18 @@
 @section('body')
 <div id="el">
     <div class="flex flex-col">
-        <div>
-            <div v-for="item in todoItems">
-                @{{ item.title }}
+        <div class="mb-2">
+            <div v-for="(item, itemInd) in todoItems" class="flex flex-row border border-gray-500 p-1" :class="roundedClass(itemInd)">
+                <div class="grow flex-col">
+                    <h4>@{{ item.title }}</h4>
+                    <hr class="block"/>
+                    <p>@{{ item.description }}</p>
+                </div>
+                <div>
+                    <button class="bg-gray-400 rounded">
+                        <i class="fas fa-times m-1"></i>
+                    </button>
+                </div>
             </div>
         </div>
         <a class="bg-red-400 p-4 rounded" href="/todos/upsert">Add a todo item</a>
@@ -20,6 +29,19 @@
         data: {
             todoItems: @json($todoItems),
         },
+        methods: {
+            roundedClass(itemInd) {
+                if (itemInd === 0) {
+                    return 'rounded-t'
+                }
+
+                if (itemInd === this.todoItems.length - 1) {
+                    return 'rounded-b border-t-0'
+                }
+
+                return 'border-t-0'
+            }
+        }
     })
 </script>
 @endsection
